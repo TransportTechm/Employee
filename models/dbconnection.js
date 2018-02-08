007ee45e7247079ae70f962e22c34d751d4c5d38
TransportTechm <p2pr.digital@gmail.com>
2018-02-09 00:15:52 +0530

    db changes

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,13 +1,34 @@
 var mysql = require('mysql');
 
 //start mysql connection
-var mysql=require('mysql');
-var connection=mysql.createPool({
-host:'localhost',
-user:'root',
-password:'root',
-database:'employee'
-});
+if (process.env.VCAP_SERVICES) {
+    var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+    var url = vcap_services.cleardb[0].credentials.uri;
+    mysqlSchema=vcap_services.cleardb[0].credentials.name;
+    console.log("PARDHU MQL"+url);
+    // mysql db Connection
+    connection = mysql.createPool({
+        host: vcap_services.cleardb[0].credentials.hostname,
+        user: vcap_services.cleardb[0].credentials.username,
+        password: vcap_services.cleardb[0].credentials.password,
+        database: vcap_services.cleardb[0].credentials.name,
+        connectionLimit: 4,
+        queueLimit: 30,
+        acquireTimeout: 10000
+    });
+}
+else {
+    // mysql db Connection
+    connection = mysql.createPool({
+        host: 'localhost',
+		user: 'root',
+        password: 'root',
+        database: 'employee',
+        connectionLimit: 4,
+        rqueueLimit: 30,
+        acquireTimeout: 10000
+    });
+}
 //end mysql connection
 
 module.exports=connection;
