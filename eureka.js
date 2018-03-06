828beba6f9340ecce330f053d2706fc2494c1f9e
TransportTechm <p2pr.digital@gmail.com>
2018-03-06 13:59:26 +0530

    Auto Scale

--- eureka.js
+++ eureka.js
@@ -2,31 +2,31 @@ const Eureka = require('pcf-eureka-client').Eureka;
 
 const client = new Eureka({
   instance: {
-    app: 'empstechm',
-    hostName: 'employeeservicetechm.cfapps.io',
+    app: 'employeeservice',
+    hostName: 'employeeservice-techm-test.cfapps.io',
     ipAddr: '127.0.0.1',
-    statusPageUrl: 'http://employeeservicetechm.cfapps.io',
+    statusPageUrl: 'http://employeeservice-techm-test.cfapps.io',
     port: {
       '$': 8080,
       '@enabled': 'true',
     },
-    vipAddress: 'empstechm',
+    vipAddress: 'employeeservice',
     dataCenterInfo: {
       '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
       name: 'MyOwn',
     },
   },
   eureka: {
-    serviceUrl: [  'https://eureka-59b65872-3262-41f4-b933-c749754ad4b7.cfapps.io/eureka/apps/' ],
+    serviceUrl: [  'https://eureka-6533d206-690d-46be-86ab-b03f55207dd0.cfapps.io/eureka/apps/' ],
   },
   
   oauth2: {
 
       clientCredentials: {
 
-          client_id: 'p-service-registry-c4239cfd-788f-485a-8343-b09f6ff7c183',
+          client_id: 'p-service-registry-324a9917-edf9-4a3b-b961-dc4d7b24b75c',
 
-          client_secret: 'REYdIED2JPDn',
+          client_secret: 'cMl5gpzwesY0',
 
          access_token_uri: 'https://p-spring-cloud-services.uaa.run.pivotal.io/oauth/token'
 
